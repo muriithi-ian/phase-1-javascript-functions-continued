bab9ebc26319413c27e72ac7d7a54d33318e4391
Muriithi Ian <imuriithiian@gmail.com>
2022-08-16 17:07:37 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+	return "This Saturday, I want to " + activity + "!";
+}
+
+function mondayWork(activity = "go to the office") {
+	return "This Monday, I will " + activity + ".";
+}
+
+function wrapAdjective(adjective) {
+	if (adjective === "*") {
+		return function (text) {
+			return `You are ${adjective}${text}${adjective}!`;
+		};
+	} else if (adjective === "||") {
+		return function (text) {
+			return `You are ${adjective}${text}${adjective}!`;
+		};
+	}
+}
